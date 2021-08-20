c2d13cdd3290dd7322b374570f2b9df19e55c548
robin-hassan <robiul.hassan12102@gmail.com>
2021-08-21 00:46:58 +0800

    resolve helm chart issue

--- stock-api/src/test/java/com/stock/stockapi/controller/StockControllerTest.java
+++ stock-api/src/test/java/com/stock/stockapi/controller/StockControllerTest.java
@@ -18,7 +18,8 @@ import java.util.List;
 
 import static com.stock.stockapi.helper.Util.fromJson;
 import static com.stock.stockapi.helper.Util.toJson;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
