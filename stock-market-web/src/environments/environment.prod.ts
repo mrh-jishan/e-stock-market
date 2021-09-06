79f0efd113108119be7ca82cb988be778c6a60d3
robin-hassan <robiul.hassan12102@gmail.com>
2021-09-07 01:31:33 +0800

    fix prod build for ui

--- stock-market-web/src/environments/environment.prod.ts
+++ stock-market-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://a55be8c4df8544fa8ae04093895a29fa-1652867620.us-east-2.elb.amazonaws.com:8080"
+  apiUrl: "https://dxfwhw3slso8x.cloudfront.net"
 };
